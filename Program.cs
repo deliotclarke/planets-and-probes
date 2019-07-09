6bad52735d520db90abc1b4a9cea241f6439127f
Daniel Clarke <deliotclarke@gmail.com>
2019-07-09 15:49:41 -0500

    exercise works and prints as it should. didn't do every planet. BUT! it does work the way it should

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PlanetsAndProbes
 {
@@ -6,7 +7,38 @@ namespace PlanetsAndProbes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> planetList = new List<string>() { "Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune" };
+
+            var probeDestinations = new List<KeyValuePair<string, string>>(){
+                new KeyValuePair<string, string>("Mercury", "Mariner 10"),
+                new KeyValuePair<string, string>("Mercury", "Messenger"),
+                new KeyValuePair<string, string>("Venus", "Sputnik 19"),
+                new KeyValuePair<string, string>("Venus", "Mariner 2"),
+                new KeyValuePair<string, string>("Venus", "Zond 1")
+            };
+
+            // Iterate planets
+            foreach (string planet in planetList)
+            {
+                // List to store probes that visited the planet
+                List<string> matchingProbes = new List<string>();
+
+                // Iterate probeDestinations
+                foreach (KeyValuePair<string, string> probeDesignation in probeDestinations)
+                {
+                    if (probeDesignation.Key == planet)
+                    {
+                        matchingProbes.Add(probeDesignation.Value);
+                    }
+                }
+
+                /*
+                    Use String.Join(",", matchingProbes) as part of the
+                    solution to get the output below. It's the C# way of
+                    writing `array.join(",")` in JavaScript.
+                */
+                Console.WriteLine($"{planet}: {String.Join(", ", matchingProbes)}");
+            }
         }
     }
 }
