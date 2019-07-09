7a827c7e08c0b7cb80d00b005ef301979afe4a06
Daniel Clarke <deliotclarke@gmail.com>
2019-07-09 15:22:06 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace planets_and_probes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
